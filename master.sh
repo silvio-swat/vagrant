a20369ce0fbd1aac293cc1b58a1dfbbec186580e
detonador31 <koomakai@gmail.com>
2025-04-27 23:55:30 -0300

    Meu primeiro commit

--- master.sh
+++ master.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 sudo docker swarm init --advertise-addr=10.10.10.100
 sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
+
+docker volume create app
+cd /var/lib/docker/volumes/app/_data
+docker service create --name meu-app --replicas 15 -dt -p 80:80 --mount type=volume,src=app,dst=/usr/local/apache2/htdocs/ httpd
--- worker.sh
+++ worker.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-3pj8k0i4tn77bd93a0yxhgh36hxuef5q5oyg1732rztnfy29ll-a94q0ipwgrjs4xikzyb4yb3n5 10.10.10.100:2377
+    docker swarm join --token SWMTKN-1-0wf764rqhoexkg7kn4m1b56c239jfsj3q97ya4psb0lsn7oydt-1fa41pv52t2wfvvd8xr2mbquc 10.10.10.100:2377
